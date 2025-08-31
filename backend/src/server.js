a45f655b4fd38d2890666c8c67b5e919ab8f2782
Naman  Singh <naman.singh.254@gmail.com>
2025-08-31 20:24:29 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { ENV } from "./config/env.js";
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("Hello from backend");
+});
+
+
+console.log("mongo uri:", ENV.MONGO_URI);
+
+app.listen(ENV.PORT, () => {
+    console.log("Server is running on port:", ENV.PORT);
+});
