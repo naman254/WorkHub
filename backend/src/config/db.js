8b53e97ecbd9bd0915d0d1bf3efc5d66a13a7d6e
Naman  Singh <naman.singh.254@gmail.com>
2025-09-02 01:19:01 +0530

    minor fix in db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,7 +4,7 @@ import {ENV} from "./env.js"
 export const connectDB = async () => {
     try {
         await mongoose.connect(ENV.MONGO_URI);
-        console.log("MongoDB Connected successfully", conn.connection.host);
+        console.log("MongoDB Connected successfully");
     } catch (error) {
         console.log("Error connecting to MongoDB:", error);
         process.exit(1);
