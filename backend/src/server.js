196ea31bf87e89cca8e85ee7b54b8b8a9ce11974
Naman  Singh <naman.singh.254@gmail.com>
2025-09-01 22:08:50 +0530

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,37 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import {connectDB} from "./config/db.js";
+import {clerkMiddleware} from "@clerk/clerk-sdk-node";
+import {inngest, functions} from "./config/inngest.js";
+import {serve} from "@inngest/express";
 
 const app = express();
+
+app.use(express.json());
+
+app.use(clerkMiddleware());
+
+app.use("/api/inngest", serve({client: inngest, functions}));
 
 
 app.get("/", (req, res) => {
     res.send("Hello from backend");
 });
 
+const startServer = async () => {
+    try {
+        await connectDB();
+       if(ENV.NODE_ENV !== "production") {
+           app.listen(ENV.PORT, () => {
+               console.log(`Server running on port ${ENV.PORT}`);
+           })
+       }
+    } catch (error) {
+        console.log("Error connecting to server:", error);
+        process.exit(1);
+    }
+};
 
+startServer();
 
-app.listen(ENV.PORT, () => {
-    console.log("Server is running on port:", ENV.PORT);
-    connectDB();
-});
+export default app;
