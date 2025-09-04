bc26f5f2af2b71f6d3fb4ad2a27bf134055abf3e
Naman  Singh <naman.singh.254@gmail.com>
2025-09-04 14:25:44 +0530

    minor fix in inngest.js

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -2,7 +2,7 @@ import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import {User} from "../models/user.model.js";
 import { upsertStreamUser } from "./stream.js";
-
+import { deleteStreamUser } from "./stream.js";
 
 
 export const inngest = new Inngest({ id: "WorkHub" });
